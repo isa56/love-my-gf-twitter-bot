dda6bb4b3791c52d247e2796e2861ab0b138d5dc
isa56 <isadoraadriana@gmail.com>
2022-03-18 19:33:54 -0300

    Adding 5 more phrases and correcting the numbers™

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function action() {
         "Babe, you're so so so beautiful, I love you soooo much <3",
         "I think my Girlfriend™ is the most amazing and dedicated person in the world",
         "I think my significant other is such a special person, they do so so much for me and understand me even when I'm unreasonable.",
-        "My SO is amazing and I'd do anything to make them happy (including coding a bot in a language I don't know)",
+        "My Significant Other is amazing and I'd do anything to make them happy (including coding a bot in a language I don't know)",
         "I wanna hug and be with them so much :D I think they're The One™",
         "You know that 'special someone'? Well, I found mine <3",
         "I really really really wanna hug them 24/7, they're so warm and comfy",
@@ -39,10 +39,15 @@ function action() {
         "My bae's so smart it makes me >//< I love it when they talk about anything they're interested in",
         "I love seeing my sweetheart doing the things they love, be it gaming, reading or nerding",
         "Every single minute being with you has been worth it",
-        "Hey, V. Thank you for these 1 year and 7 months. It means the world to me.",
-        "It's been more than 500 days with my love. Not all have been perfect, but it's all worth it because I have them.",
+        "Hey, V. Thank you for these 1 year and 8 months. It means the world to me.",
+        "It's been more than 600 days with my love. Not all have been perfect, but it's all worth it because I have them.",
         "I would sword fight while flirting with you anytime",
         "Hey, I can help you study the chemistry that exists between us, dear",
+        "Hey, love, I'm here just to tell you how much I love you.",
+        "Would you swordplay with me?",
+        "*big mouth* ᵃ",
+        "All I wanna do is wake up next to you every single day",
+        "I would love to be with you 24/7, my heart",
     ];
 
     const position = Math.floor(Math.random() * 23);
